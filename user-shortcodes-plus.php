271380d6f3affbcb06272d91b7ad50c32969684b
Kyle B. Johnson <kyle@wpninjas.com>
2016-04-14 20:45:05 -0400

    add user avatar shortcodes.

--- user-shortcodes-plus.php
+++ user-shortcodes-plus.php
@@ -45,6 +45,9 @@ final class KBJ_UserShortcodesPlus
             add_shortcode( $tag, array( $this, 'get_userdata' ) );
         }
         add_shortcode( 'user_meta', array( $this, 'get_user_meta' ) );
+
+        add_shortcode( 'user_avatar',     array( $this, 'user_avatar'     ) );
+        add_shortcode( 'user_avatar_url', array( $this, 'user_avatar_url' ) );
     }
 
     public function get_userdata( $atts, $content, $tag )
@@ -67,6 +70,29 @@ final class KBJ_UserShortcodesPlus
         ), $atts ));
         
         return ( $key ) ? get_user_meta( $id, $key, TRUE ) : '';
+    }
+
+    public function user_avatar( $atts )
+    {
+        extract( shortcode_atts( array(
+            'id' => get_current_user_id(),
+        ), $atts ));
+
+        $userdata = get_userdata( $id );
+
+        return get_avatar( $userdata->user_email );
+    }
+
+    public function user_avatar_url( $atts )
+    {
+        extract( shortcode_atts( array(
+            'id' => get_current_user_id(),
+            'size' => 500
+        ), $atts ));
+
+        $userdata = get_userdata( $id );
+
+        return get_avatar_url( $userdata->user_email, array( 'size' => $size ) );
     }
 }
 
