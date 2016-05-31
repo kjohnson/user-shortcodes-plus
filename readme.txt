7afae2fb7bed5b8ec202ba14eae8ade6480d93d8
Kyle B. Johnson <kyle@wpninjas.com>
2016-05-31 00:27:26 -0400

    rewrite.

--- readme.txt
+++ readme.txt
@@ -2,9 +2,9 @@
 Contributors: kbjohnson90
 Tags: user, shortcodes, meta
 Donate link: http://kylebjohnson.me/plugins
-Requires at least: 4.2
-Tested up to: 4.5
-Stable tag: 1.0.1
+Requires at least: 4.3
+Tested up to: 4.5.2
+Stable tag: 2.0.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -14,17 +14,21 @@ Add simple user shortcodes to WordPress for displaying information, including cu
 Add simple user shortcodes to WordPress for displaying information, including custom meta and avatars, for any user.
 
 Available Shortcodes:
-* [user_id]
-* [user_login]
-* [user_email]
-* [user_firstname]
-* [user_lastname]
-* [user_nicename]
-* [user_display]
-* [user_display_name]
-* [user_registered]
-* [user_avatar]
-* [user_avatar_url]
+- [user_id]
+- [user_login]
+- [user_email]
+- [user_firstname]
+- [user_lastname]
+- [user_nicename]
+- [user_display]
+- [user_display_name] (alias)
+- [user_registered]
+- [user_avatar] (image)
+- [user_avatar_url]
+- [user_url]
+- [user_website] (alias)
+- [user_description]
+- [user_bio] (alias)
 
 Displaying userdata for another user:
 
@@ -34,7 +38,7 @@ ex. [user_email id=2]
 This section describes how to install the plugin and get it working.
 
 1. Upload the `user-shortcodes-plus` directory to your `/wp-content/plugins/` directory
-2. Activate the plugin through the \'Plugins\' menu in WordPress
+2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Add shortcodes to the content of any post type.
 
 == Frequently Asked Questions ==
@@ -47,19 +51,41 @@ What shortcodes are supported?
 - [user_lastname]
 - [user_nicename]
 - [user_display]
-- [user_display_name]
+- [user_display_name] (alias)
 - [user_registered]
-- [user_avatar]
+- [user_avatar] (image)
 - [user_avatar_url]
+- [user_url]
+- [user_website] (alias)
+- [user_description]
+- [user_bio] (alias)
 
 == Screenshots ==
-1. Example use of shortcodes
+
+1. Shortcodes can be added to any post type via the content editor.
+2. Each shortcode is replaced with the user's data, when available.
+3. Shortcodes can be inserted using the 'Add User Shortcode' button on the TinyMCE editor.
+4. A specific user cna be specified by adding the 'id' attribute to the shortcode.
+5. Any user metadata can be displayed using the [user_meta] shortcode with a specified 'key' attribute.
 
 == Changelog ==
 
+= 2.0.0 =
+* Add support for [user_url] and [user_website] shortcodes.
+* Add support for [user_description] and [user_bio] shortcodes.
+* Add TinyMCE editor button for easily adding shortcodes.
+* Restructure plugin for extendability.
+
 = 1.0.1 =
+* Add support for [user_avatar] and [user_avatar_url].
 
-Add support for [user_avatar] and [user_avatar_url].
 = 1.0.0 =
+* Initial Commit
 
-Initial Commit
+== Upgrade Notice ==
+
+= 2.0.0 =
+Added new shortcodes for the user's website and bio. Also added a TinyMCE button.
+
+= 1.0.1 =
+Added new shortcodes for the user's avatar.
--- user-shortcodes-plus.php
+++ user-shortcodes-plus.php
@@ -4,7 +4,7 @@
  * Plugin Name: User Shortcodes Plus
  * Plugin URI: http://kylebjohnson.me/plugins
  * Description: Add simple user shortcodes to WordPress for displaying information, including <strong>custom meta</strong> and <strong>avatars</strong>, for any user.
- * Version: 1.0.1
+ * Version: 2.0.0
  * Author: Kyle B. Johnson
  * Author URI: http://kylebjohnson.me
  * Text Domain: user-shortcodes-plus
@@ -14,86 +14,100 @@
 
 final class KBJ_UserShortcodesPlus
 {
-    /**
-     * @var array $userdata[ user_id ][ property ]
-     */
-    private $userdata = array();
+    const VERSION = '2.0.0';
 
-    /**
-     * @var array $shortcodes[ tag ] => userdata
-     */
-    private $shortcodes = array(
-        'user_id'           => 'ID',
-        'user_login'        => 'user_login',
-        'user_email'        => 'user_email',
-        'user_firstname'    => 'first_name',
-        'user_lastname'     => 'last_name',
-        'user_nicename'     => 'user_nicename',
-        'user_display'      => 'display_name',
-        'user_display_name' => 'display_name',
-        'user_registered'   => 'user_registered'
-    );
+    const PREFIX  = 'KBJ_UserShortcodesPlus';
+
+    private static $instance;
+
+    public static $dir = '';
+
+    public static $url = '';
 
     public function __construct()
     {
         add_action( 'init', array( $this, 'init' ) );
+        add_action( 'admin_init', array( $this, 'admin_init' ) );
+        add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
     }
 
     public function init()
     {
-        foreach( $this->shortcodes as $tag => $property ) {
-            add_shortcode( $tag, array( $this, 'get_userdata' ) );
+        new KBJ_UserShortcodesPlus_Shortcodes_User();
+        new KBJ_UserShortcodesPlus_Shortcodes_UserMeta();
+        new KBJ_UserShortcodesPlus_Shortcodes_UserAvatar();
+    }
+
+    public function admin_init()
+    {
+        new KBJ_UserShortcodesPlus_Admin_TinyMCE_AddShortcodeButton();
+    }
+
+    public function admin_enqueue_scripts()
+    {
+        wp_register_style( 'kbj_user_shortcodes_plus_admin_css', self::url( 'assets/css/admin.css' ), FALSE, self::VERSION );
+        wp_enqueue_style( 'kbj_user_shortcodes_plus_admin_css' );
+
+        wp_register_script( 'kbj_user_shortcodes_plus_admin_js', self::url( 'assets/js/admin.js' ), array( 'jquery' ), self::VERSION );
+        wp_enqueue_script( 'kbj_user_shortcodes_plus_admin_js' );
+    }
+
+    public static function instance()
+    {
+        if ( ! isset(self::$instance) && ! ( self::$instance instanceof KBJ_UserShortcodesPlus ) ) {
+            self::$instance = new KBJ_UserShortcodesPlus();
+            self::$dir = plugin_dir_path(__FILE__);
+            self::$url = plugin_dir_url(__FILE__);
+            spl_autoload_register( array( self::$instance, 'autoloader' ) );
         }
-        add_shortcode( 'user_meta', array( $this, 'get_user_meta' ) );
-
-        add_shortcode( 'user_avatar',     array( $this, 'user_avatar'     ) );
-        add_shortcode( 'user_avatar_url', array( $this, 'user_avatar_url' ) );
+        return self::$instance;
     }
 
-    public function get_userdata( $atts, $content, $tag )
+    public static function dir( $path = '' )
     {
-        extract( shortcode_atts( array(
-            'id' => get_current_user_id(),
-        ), $atts ));
-
-        $property = $this->shortcodes[ $tag ];
-        $userdata = ( isset( $this->userdata[ $id ] ) ) ? $this->userdata[ $id ] : get_userdata( $id );
-
-        return $userdata->$property;
+        return self::$dir . $path;
     }
 
-    public function get_user_meta( $atts )
+    public static function url( $url = '' )
     {
-        extract( shortcode_atts( array(
-            'id' => get_current_user_id(),
-            'key' => '',
-        ), $atts ));
-        
-        return ( $key ) ? get_user_meta( $id, $key, TRUE ) : '';
+        return self::$url . $url;
     }
 
-    public function user_avatar( $atts )
+    public static function autoloader( $class_name )
     {
-        extract( shortcode_atts( array(
-            'id' => get_current_user_id(),
-        ), $atts ));
+        if( class_exists( $class_name ) ) return;
 
-        $userdata = get_userdata( $id );
+        if( false === strpos( $class_name, self::PREFIX ) ) return;
 
-        return get_avatar( $userdata->user_email );
+        $class_name = str_replace( self::PREFIX, '', $class_name );
+        $classes_dir = realpath(plugin_dir_path(__FILE__)) . DIRECTORY_SEPARATOR . 'includes' . DIRECTORY_SEPARATOR;
+        $class_file = str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+
+        if (file_exists($classes_dir . $class_file)) {
+            require_once $classes_dir . $class_file;
+        }
     }
 
-    public function user_avatar_url( $atts )
+    public static function config( $file_name )
     {
-        extract( shortcode_atts( array(
-            'id' => get_current_user_id(),
-            'size' => 500
-        ), $atts ));
+        return include self::dir( 'includes/Config/' . $file_name . '.php' );
+    }
 
-        $userdata = get_userdata( $id );
+    public static function template( $file_name = '', array $data = array() )
+    {
+        if( ! $file_name ) return '';
 
-        return get_avatar_url( $userdata->user_email, array( 'size' => $size ) );
+        extract( $data );
+
+        ob_start();
+        include self::dir( 'includes/Templates/' . $file_name );
+        return ob_get_clean();
     }
 }
 
-new KBJ_UserShortcodesPlus();
+function KBJ_UserShortcodesPlus()
+{
+    return KBJ_UserShortcodesPlus::instance();
+}
+
+KBJ_UserShortcodesPlus();
